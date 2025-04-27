6d91fbc7d16711744797b0c9e0ee9b62ee6b0c01
Elliot <unforriven@gmail.com>
2025-04-27 14:09:33 -0300

    form to add player

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { CardPlayer } from './components/CardPlayer.jsx'
 
 function App() {
 
   const [players, setPlayers] = useState([])
   const [onReset, setOnReset] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+
+  const { register, handleSubmit } = useForm()
 
   useEffect(() => {
     setPlayers([
@@ -26,10 +30,37 @@ function App() {
     }
   }
 
+  // const handleAddPlayer = () => {
+
+  // }
+
   return (
     <>
       <h1 className=" text-amber-800 text-2xl text-center my-4">UltraMunchkin</h1>
-      <button onClick={() => handleReset()} className='px-4 py-2 border border-gray-950 text-white rounded-full mx-4 cursor-pointer hover:bg-gray-800'>Reset</button>
+      <button onClick={() => handleReset()} className='p-2 hover:bg-zinc-700 ml-4 text-white  cursor-pointer'>Reset</button>
+      <button onClick={() => setShowModal(prev => !prev)} className='cursor-pointer hover:bg-zinc-700 p-2'>Agregar Jugador</button>
+      <section className={`bg-[#0f0f0f86] absolute ${showModal ? 'flex' : 'hidden'} justify-center items-center w-[80%] h-[60%] z-10 top-0 bottom-0 left-0 right-0 m-auto rounded-md`}>
+        <button className='absolute top-5 right-5 rounded-full text-red-300 px-2 cursor-pointer border border-red-600' onClick={() => setShowModal(prev => !prev)}>X</button>
+        <form className=' flex flex-col justify-center items-start gap-4'
+          onSubmit={handleSubmit((values) => {
+            console.log(values)
+            setShowModal(prev => !prev)
+          })}>
+          <h2 className='text-center p-2 m-auto'>Agregar nuevo Jugador</h2>
+          <label htmlFor="nombre">Nombre:</label>
+          <input type="text" placeholder='Nombre o Alias' className='p-2 m-2' name='nombre' id='nombre' {...register('nombre')} />
+
+          <div className='flex gap-2'>
+            <label htmlFor="sex">Sexo</label>
+            <select name="sex" id="" {...register('sex')}>
+              <option value="femenino">♀️</option>
+              <option value="masculino">♂️</option>
+            </select>
+          </div>
+
+          <button>Agregar</button>
+        </form>
+      </section>
       <div className="flex flex-col gap-4 max-w-[90%] m-auto">
         {players.map((player, index) => {
           return (
